3424ef02cf18210d67b7e97bb3353ae9781c6063
Ruan <oliveiraruan2018@gmail.com>
2025-08-12 11:16:28 -0400

    fix: replacement word return for throw and adjusting in parameter email

--- src/application/services/AuthService.js
+++ src/application/services/AuthService.js
@@ -2,33 +2,33 @@ import UserRespository from "../../domain/repositorys/UserRespository.js";
 import AppError from "../../shared/error/AppError.js";
 import jwt from 'jsonwebtoken';
 import authConfig from '../../shared/config/auth.js';
+import UserValidator from "../validations/UserValidator.js";
 
 class AuthService{
     static async register({ name, email, password}){
 
         if(!name || !email || !password){
-            return new AppError('fields incompleted', 400);
+            throw new AppError('fields incompleted', 400);
         }
 
-        if(await UserRespository.findByEmail({ email })){
-            return new AppError('user exists', 404);
+        if (await UserValidator.isExistsByEmail(email)) {
+            throw new AppError('user with this email already registered', 409);
         }
 
         await UserRespository.save({ name, email, password});
     }
     static async login(data){
 
-        const user = await UserRespository.findByEmail(data);
+        const user = await UserRespository.findByEmail(data.email);
 
         if(!user){
-            console.log(user);
-            return new AppError('user not exists', 404);
+            throw new AppError('user not exists', 404);
         }
 
         const userVerified = await UserRespository.findByEmailAndComparePassword(data);
         
         if(!userVerified){
-            return new AppError('password incorrect', 400);
+            throw new AppError('password incorrect', 400);
         }
 
         const { id, name, email}= user;
