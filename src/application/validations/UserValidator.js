48665773e2dfc17aa65b1a78cd03aa0ae519fab2
Ruan <oliveiraruan2018@gmail.com>
2025-08-12 11:17:23 -0400

    feat: add validation of user, verification if exists by email

--- src/application/validations/UserValidator.js
+++ src/application/validations/UserValidator.js
@@ -4,6 +4,9 @@ class UserValidator{
     static async isExists(id) {
         return !(await UserRepository.findById(id)) ? false : true;
     }
+    static async isExistsByEmail(email) {
+        return !(await UserRepository.findByEmail(email)) ? false : true;
+    }
 }
 
 export default UserValidator;
