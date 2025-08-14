98fcb153e7d1889ef07265e9bd1b406830e333e4
Ruan <oliveiraruan2018@gmail.com>
2025-08-14 15:43:14 -0400

    feat: middleware for check query params of router /favorites

---
+++ src/http/middlewares/checkTypeFavorite.js
@@ -0,0 +1,13 @@
+export default async (req, res, next) => {
+    const types = ['project', 'material'];
+
+    const queryParams = Object.values(req.query);
+
+    if (queryParams.length === 0) {
+        return next();
+    }else if (!(queryParams.length === 1 && types.includes(queryParams[0]))) {
+        return res.status(400).json({ message: 'incorrect query params' });
+    }
+
+    return next();
+}
