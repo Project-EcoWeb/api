26dd401b32849fd318a0d06ec5832aec356aab8c
Ruan <oliveiraruan2018@gmail.com>
2025-08-20 11:56:09 -0400

    fix: remove file of routes favorites

--- src/http/routes/favoriteRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import FavoriteController from "../controllers/FavoriteController.js";
-import checkTypeFavorite from "../middlewares/checkTypeFavorite.js";
-
-const router = Router();
-
-router.get('/', checkTypeFavorite, FavoriteController.getAllByUser);
-
-export default router;
