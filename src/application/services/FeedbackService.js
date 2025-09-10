dd19468ac88a32ce8d1cf8b4f989d1ff5a69ac2e
Ruan <oliveiraruan2018@gmail.com>
2025-09-10 12:40:46 -0400

    feat: logic the save feedback, and implemented  in all layers

--- src/application/services/FeedbackService.js
+++ src/application/services/FeedbackService.js
@@ -1,7 +1,29 @@
 import FeedbackRepository from "../../domain/repositorys/FeedbackRepository";
+import AppError from "../../shared/error/AppError";
+import MaterialValidator from "../validations/MaterialValidator.js";
+import UserValidator from "../validations/UserValidator.js";
+import MaterialService from "../services/MaterialService.js";
 
 class FeedbackService{
+    static async save(data) {
+        if (!(await MaterialValidator.isExists(data.material))) {
+            throw new AppError('material is not exists', 403);
+        }
 
+        if (!(await UserValidator.isExists(data.receiver))) {
+            throw new AppError('user not exists', 403);
+        }
+
+        /**
+         * Pendente criar colecção de fornecedores
+         */
+
+        const { material } = await FeedbackRepository.create(data);
+        
+        /**
+         * Pendente atualizar o status material automaticamente
+         */
+    }
 }
 
 export default FeedbackService;
--- src/domain/model/Feedback.js
+++ src/domain/model/Feedback.js
@@ -2,17 +2,17 @@ import { model, Schema } from 'mongoose';
 
 
 const feedbackSchema = new Schema({
-    materialId: {
+    material: {
         type: Schema.Types.ObjectId,
         ref: 'Material',
         required: [true, 'id material is required']
     },
-    idUserSupplier: {
+    supplier: {
         type: Schema.Types.ObjectId,
         ref: 'User',
         required: [true, 'id user supplier is required']
     },
-    idUser: {
+    receiver: {
         type: Schema.Types.ObjectId,
         ref: 'User',
         required: [true, 'id user receiving is required']
