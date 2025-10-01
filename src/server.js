47055fe0e9fae17072587e0bf7d59bf885b44aba
Ruan <oliveiraruan2018@gmail.com>
2025-10-01 14:42:43 -0400

    fix: replace port server[pendind encapsule in env]

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import { app } from './app.js';
 import logger from "./infra/logger/logger.js";
 
-const PORT_SERVER = 3000
+const PORT_SERVER = 3333
 app.listen(PORT_SERVER, () => {
     logger.info(`${new Date().toISOString()}: Server Running at http://localhost:${PORT_SERVER}`);
 });
