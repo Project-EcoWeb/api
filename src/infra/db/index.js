979cf452f8a77aafc91f9792319ae5bceb2ce5a1
Ruan <oliveiraruan2018@gmail.com>
2026-01-19 15:46:03 -0400

    fix!: add authentication mongoose and remove version docker compose

--- src/infra/db/index.js
+++ src/infra/db/index.js
@@ -3,6 +3,10 @@ import dotenv from 'dotenv';
 import logger from '../logger/logger.js';
 
 dotenv.config();
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL, {
+    auth: {
+        username: process.env.MONGODB_USERNAME,
+        password: process.env.MONGODB_PASSWORD
+}})
     .then(() => logger.info(`${new Date().toISOString()}: database connected`))
     .catch(() => logger.info(`${new Date().toISOString()}: database connection failed`));
