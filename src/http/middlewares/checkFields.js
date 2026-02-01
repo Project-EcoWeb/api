f9c34adfd1a9c71a1f31f6e5e7eb0aa0d03c6be9
Ruan <oliveiraruan2018@gmail.com>
2026-02-01 19:44:14 -0400

    fix: adding verification fields body request

--- src/http/middlewares/checkFields.js
+++ src/http/middlewares/checkFields.js
@@ -2,6 +2,12 @@ export default async (req, res, next) => {
 
 
     if (req.query.q === 'company') {
+        const fields = ['emailOrCnpj', 'password'];
+        fields.forEach((field) => {
+            if (!req.body[field] && fields.includes(field)) {
+                return res.status(400).json({ message: `missing ${field} field` });
+            }
+        })
         return next();
     }
 
