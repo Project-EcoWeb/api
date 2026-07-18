51ab1d50e717ad1ed4c59e71da6372b3f2794eeb
Ruan Oliveira de Almeida <oliveiraruan2018@gmail.com>
2026-07-18 19:32:04 -0400

    fix: refactor field validation to use for..of loop for consistency

--- src/http/middlewares/checkFields.js
+++ src/http/middlewares/checkFields.js
@@ -3,22 +3,22 @@ export default async (req, res, next) => {
 
     if (req.query.q === 'company') {
         const fields = ['emailOrCnpj', 'password'];
-        fields.forEach((field) => {
-            if (!req.body[field] && fields.includes(field)) {
+        for (const field of fields) {
+            if (!req.body[field]) {
                 return res.status(400).json({ message: `missing ${field} field` });
             }
-        })
+        }
         return next();
     }
 
     const fields = ['email', 'password'];
 
-    fields.forEach((field) => {
-        if (!req.body[field] && fields.includes(field)) {
+    for (const field of fields) {
+        if (!req.body[field]) {
             return res.status(400).json({ message: `missing ${field} field` });
         }
-    })
+    }
 
     return next();
     
-} 
+}
