34a1e0688cb28cf98e38f912d6522b65e7a442b4
Ruan Oliveira de Almeida <oliveiraruan2018@gmail.com>
2026-07-25 10:28:07 -0400

    fix: add missing newline at the end of companyRoutes.js

--- src/http/routes/companyRoutes.js
+++ src/http/routes/companyRoutes.js
@@ -4,7 +4,8 @@ import CompanyController from "../controllers/CompanyController.js";
 
 const router = Router();
 
+router.patch('/', CompanyController.update);
 router.get('/me/profile', CompanyController.getMeProfile);
 router.get('/:id/profile', CompanyController.getProfile);
 
-export default router; 
+export default router;
